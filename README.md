6f7abd0657d260c8b42342c495bf724f63f114aa
David Griffin <david.dtjg2001@gmail.com>
2022-01-30 22:51:10 -0700

    added color coding to hours based on current hour

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Work Day Scheduler Starter Code
+## Description
+- I created this project for people that need to schedule out their work day
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+Open the link: https://davidtjgriffin.github.io/schedule-planner/
+## Usage
+Click the various time blocks for each hours and type in your tasks. 
+
+
+
+
+![Website Screenshot](assets/images/website-screenshot.jpg)
+## Credits
+[David Griffin](https://github.com/Ukarimu1)
+## License
+MIT License
+
+Copyright © 2021 David Griffin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+![MIT License](https://img.shields.io/apm/l/vim-mode)
+
+## Features
+- Color-coded hours based on if hour is in past, present or future
+- Dynamically change text of time blocks
+## How to Contribute
+1. Fork the project to your own machine
+2. Refactor code to be further optimized 
+3. Commit changes to your own branch
+4. Push your work back up to your branch
+5. Submit a Pull request so that I can review your changes
+## Tests
+- Enter in text for each time block. 
+- Wait for the hours of the days to change and observe the colors changing
--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,144 @@ $(".time-block").on("blur", "textarea", function () {
     // replace textarea with p element
     $(this).replaceWith(taskP);
 });
+
+
+var currentTime = moment().hour();
+console.log(currentTime);
+
+
+
+var changeColor = function () {
+if (currentTime > 17) {
+ document.getElementById("9am").classList.add('past');
+ document.getElementById("10am").classList.add('past');
+ document.getElementById("11am").classList.add('past');
+ document.getElementById("12pm").classList.add('past');
+ document.getElementById("1pm").classList.add('past');
+ document.getElementById("2pm").classList.add('past');
+ document.getElementById("3pm").classList.add('past');
+ document.getElementById("4pm").classList.add('past');
+ document.getElementById("5pm").classList.add('past');
+}
+else if (currentTime < 9) {
+    document.getElementById("9am").classList.add('future');
+    document.getElementById("10am").classList.add('future');
+    document.getElementById("11am").classList.add('future');
+    document.getElementById("12pm").classList.add('future');
+    document.getElementById("1pm").classList.add('future');
+    document.getElementById("2pm").classList.add('future');
+    document.getElementById("3pm").classList.add('future');
+    document.getElementById("4pm").classList.add('future');
+    document.getElementById("5pm").classList.add('future');
+}
+else if (currentTime === 9) {
+    document.getElementById("9am").classList.add('present');
+    document.getElementById("10am").classList.add('future');
+    document.getElementById("11am").classList.add('future');
+    document.getElementById("12pm").classList.add('future');
+    document.getElementById("1pm").classList.add('future');
+    document.getElementById("2pm").classList.add('future');
+    document.getElementById("3pm").classList.add('future');
+    document.getElementById("4pm").classList.add('future');
+    document.getElementById("5pm").classList.add('future');
+}
+else if (currentTime === 10) {
+    document.getElementById("9am").classList.add('past');
+    document.getElementById("10am").classList.add('present');
+    document.getElementById("11am").classList.add('future');
+    document.getElementById("12pm").classList.add('future');
+    document.getElementById("1pm").classList.add('future');
+    document.getElementById("2pm").classList.add('future');
+    document.getElementById("3pm").classList.add('future');
+    document.getElementById("4pm").classList.add('future');
+    document.getElementById("5pm").classList.add('future');
+}
+else if (currentTime === 11) {
+    document.getElementById("9am").classList.add('past');
+    document.getElementById("10am").classList.add('past');
+    document.getElementById("11am").classList.add('present');
+    document.getElementById("12pm").classList.add('future');
+    document.getElementById("1pm").classList.add('future');
+    document.getElementById("2pm").classList.add('future');
+    document.getElementById("3pm").classList.add('future');
+    document.getElementById("4pm").classList.add('future');
+    document.getElementById("5pm").classList.add('future');
+}
+else if (currentTime === 12) {
+    document.getElementById("9am").classList.add('past');
+    document.getElementById("10am").classList.add('past');
+    document.getElementById("11am").classList.add('past');
+    document.getElementById("12pm").classList.add('present');
+    document.getElementById("1pm").classList.add('future');
+    document.getElementById("2pm").classList.add('future');
+    document.getElementById("3pm").classList.add('future');
+    document.getElementById("4pm").classList.add('future');
+    document.getElementById("5pm").classList.add('future');
+}
+else if (currentTime === 13) {
+    document.getElementById("9am").classList.add('past');
+    document.getElementById("10am").classList.add('past');
+    document.getElementById("11am").classList.add('past');
+    document.getElementById("12pm").classList.add('past');
+    document.getElementById("1pm").classList.add('present');
+    document.getElementById("2pm").classList.add('future');
+    document.getElementById("3pm").classList.add('future');
+    document.getElementById("4pm").classList.add('future');
+    document.getElementById("5pm").classList.add('future');
+}
+else if (currentTime === 14) {
+    document.getElementById("9am").classList.add('past');
+    document.getElementById("10am").classList.add('past');
+    document.getElementById("11am").classList.add('past');
+    document.getElementById("12pm").classList.add('past');
+    document.getElementById("1pm").classList.add('past');
+    document.getElementById("2pm").classList.add('present');
+    document.getElementById("3pm").classList.add('future');
+    document.getElementById("4pm").classList.add('future');
+    document.getElementById("5pm").classList.add('future');
+}
+else if (currentTime === 15) {
+    document.getElementById("9am").classList.add('past');
+    document.getElementById("10am").classList.add('past');
+    document.getElementById("11am").classList.add('past');
+    document.getElementById("12pm").classList.add('past');
+    document.getElementById("1pm").classList.add('past');
+    document.getElementById("2pm").classList.add('past');
+    document.getElementById("3pm").classList.add('present');
+    document.getElementById("4pm").classList.add('future');
+    document.getElementById("5pm").classList.add('future');
+}
+else if (currentTime === 16) {
+    document.getElementById("9am").classList.add('past');
+    document.getElementById("10am").classList.add('past');
+    document.getElementById("11am").classList.add('past');
+    document.getElementById("12pm").classList.add('past');
+    document.getElementById("1pm").classList.add('past');
+    document.getElementById("2pm").classList.add('past');
+    document.getElementById("3pm").classList.add('past');
+    document.getElementById("4pm").classList.add('present');
+    document.getElementById("5pm").classList.add('future');
+}
+else if (currentTime === 17) {
+    document.getElementById("9am").classList.add('past');
+    document.getElementById("10am").classList.add('past');
+    document.getElementById("11am").classList.add('past');
+    document.getElementById("12pm").classList.add('past');
+    document.getElementById("1pm").classList.add('past');
+    document.getElementById("2pm").classList.add('past');
+    document.getElementById("3pm").classList.add('past');
+    document.getElementById("4pm").classList.add('past');
+    document.getElementById("5pm").classList.add('present');
+}
+
+};
+
+changeColor();
+// 9 = 9 
+// 10 = 10
+// 11 = 11
+// 12 = 12
+// 1 = 13
+// 2 = 14
+// 3 = 15
+// 4 = 16
